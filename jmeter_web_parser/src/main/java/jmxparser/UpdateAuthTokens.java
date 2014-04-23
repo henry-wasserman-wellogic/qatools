17efa7419f8ccc306f67084799064d38c0efe19b
Henry Wasserman <henry.wasserman@wellogic.com>
2014-04-23 12:44:05 -0400

    Fixed up reg_extracter xml and it's placement position in the new jmx file.

--- jmeter_web_parser/src/main/java/jmxparser/UpdateAuthTokens.java
+++ jmeter_web_parser/src/main/java/jmxparser/UpdateAuthTokens.java
@@ -2,6 +2,7 @@ package jmxparser;
 
 import java.io.File;
 import java.io.IOException;
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -23,18 +24,38 @@ public class UpdateAuthTokens
         
         File jmx_file = new File(args[0]);
         
-        boolean found_it = false;
+        String reg_extracter_xml =
+        		"            <RegexExtractor guiclass=\"RegexExtractorGui\" testclass=\"RegexExtractor\" testname=\"Regular Expression Extractor\" enabled=\"true\">\n" +
+        		"              <stringProp name=\"RegexExtractor.useHeaders\">false</stringProp>\n" +
+        		"              <stringProp name=\"RegexExtractor.refname\">authToken</stringProp>\n" +
+        		"              <stringProp name=\"RegexExtractor.regex\">.*\\&quot;authToken\\&quot;\\:\\&quot;([\\d|\\w]+)</stringProp>\n" +
+        		"              <stringProp name=\"RegexExtractor.template\">$1$</stringProp>\n" +
+        		"              <stringProp name=\"RegexExtractor.default\">COULD NOT EXTRACT TOKEN</stringProp>\n" +
+        		"              <stringProp name=\"RegexExtractor.match_number\"></stringProp>\n" +
+        		"            </RegexExtractor>\n" +
+        		"            <hashTree/>";
+        
+        boolean found_auth_token = false;
+        boolean found_login = false;
         try {
         	List<String> list = FileUtils.readLines(jmx_file);
+        	ArrayList<String> new_list = new ArrayList<String>();
         	
         	Pattern pattern = Pattern.compile(".*Argument\\.value\\\"\\>([\\d|\\w]+)\\<.*");
         	Matcher matcher = null;
         	String match = "";
         	HashMap<String,String> map = new HashMap<String,String>();
         	
+        	//Put all authTokens that are found into a hash in order to replace them later with the string '${authToken}'
+        	int index = -1;
+        	int new_index = -1;
         	for (String line : list) {
+        		index++;
+        		new_index++;
+        		new_list.add(line);
         		
-        		if (found_it && StringUtils.contains(line,"<stringProp name=\"Argument.value\">")) {
+        		//authTokens are put into hash here
+        		if (found_auth_token && StringUtils.contains(line,"<stringProp name=\"Argument.value\">")) {
         			matcher = pattern.matcher(line);
         			if (matcher.find()) {
         				match = matcher.group(1);
@@ -43,36 +64,49 @@ public class UpdateAuthTokens
         			else {
         				match = "MATCH NOT FOUND";
         			}
-    				found_it = false;
+    				found_auth_token = false;
     				continue;
 
         		}
         		
+        		//Regular Expression Extractor xml can be inserted here
+        		if (found_login && StringUtils.contains(line,"<hashTree>")) {
+        			new_list.add(reg_extracter_xml);
+        			found_login = false;
+        			continue;
+        		}
         		
+        		//If we see this then we can start looking for authTokens
         		if (StringUtils.contains(line, "<elementProp name=\"authToken\" elementType=\"HTTPArgument\">")) {
-        			found_it = true;
+        			found_auth_token = true;
+        			continue;
+        		}
+        		
+        		//If we see this then we can start looking for the place to insert the regular expression extractor xml.
+        		if (StringUtils.contains(line,  "<HTTPSamplerProxy guiclass=\"HttpTestSampleGui\" testclass=\"HTTPSamplerProxy\" testname=\"/login\" enabled=\"true\">")) {
+        			found_login = true;
         			continue;
         		}
         	
         	}
         	
+        	//For each authToken in the hash replace it with the string '${authToken}'
         	for (Map.Entry<String,String> entry : map.entrySet()) {
         	    System.out.print("key");
         	    System.out.println(entry.getKey());
         	
-        	    int index = 0;
-        		for (String line : list) {
+        	    index = 0;
+        		for (String line : new_list) {
         			if (line.contains(entry.getKey())) {
         				line = StringUtils.replace(line, entry.getKey(), "${authToken}",-1);
+            			new_list.set(index, line);
         			}
-        		
-        			list.set(index, line);
         			index++;
         		}
         		
         	}
         	
-        	FileUtils.writeLines(jmx_file, list);
+        	FileUtils.writeLines(jmx_file, new_list);
  
         }
         catch  (IOException e) {
