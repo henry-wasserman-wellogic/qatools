0e97a73fe8b1d62e5012bf19a0ded3598fea2430
Henry Wasserman <henry.wasserman@wellogic.com>
2014-04-29 17:36:10 -0400

    uncommenting load test.

--- jmeter_web_parser/src/main/scripts/jenkins_loadtest.sh
+++ jmeter_web_parser/src/main/scripts/jenkins_loadtest.sh
@@ -8,4 +8,4 @@ jmeter=/Applications/apache-jmeter-2.11/bin/jmeter.sh
 jmx_file_dir=$workspace/jmeter_web_parser/src/main/jmx_files
 
 echo $jmeter -n -t $HOME/$jmx_file_dir/${jmx_file}_threaded.jmx -l $workspace/$jmx_file_dir/$jmx_file.xml -Djmeter.save.saveservice.output_format=xml
-#$jmeter -n -t $HOME/$jmx_file_dir/$jmx_file_threaded.jmx  -l $workspace/$jmx_file_dir/$jmx_file.xml -Djmeter.save.saveservice.output_format=xml
+$jmeter -n -t $HOME/$jmx_file_dir/${jmx_file}_threaded.jmx -l $workspace/$jmx_file_dir/$jmx_file.xml -Djmeter.save.saveservice.output_format=xml
