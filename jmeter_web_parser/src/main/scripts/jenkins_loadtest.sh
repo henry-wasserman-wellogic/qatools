9a211d61ed37975be9c0646291e597410e36142f
Henry Wasserman <henry.wasserman@wellogic.com>
2014-04-29 17:38:12 -0400

    took out $HOME variable, not needed.

--- jmeter_web_parser/src/main/scripts/jenkins_loadtest.sh
+++ jmeter_web_parser/src/main/scripts/jenkins_loadtest.sh
@@ -7,5 +7,5 @@ jmx_file=$2
 jmeter=/Applications/apache-jmeter-2.11/bin/jmeter.sh
 jmx_file_dir=$workspace/jmeter_web_parser/src/main/jmx_files
 
-echo $jmeter -n -t $HOME/$jmx_file_dir/${jmx_file}_threaded.jmx -l $workspace/$jmx_file_dir/$jmx_file.xml -Djmeter.save.saveservice.output_format=xml
-$jmeter -n -t $HOME/$jmx_file_dir/${jmx_file}_threaded.jmx -l $workspace/$jmx_file_dir/$jmx_file.xml -Djmeter.save.saveservice.output_format=xml
+echo $jmeter -n -t $jmx_file_dir/${jmx_file}_threaded.jmx -l $workspace/$jmx_file_dir/$jmx_file.xml -Djmeter.save.saveservice.output_format=xml
+$jmeter -n -t $jmx_file_dir/${jmx_file}_threaded.jmx -l $workspace/$jmx_file_dir/$jmx_file.xml -Djmeter.save.saveservice.output_format=xml
