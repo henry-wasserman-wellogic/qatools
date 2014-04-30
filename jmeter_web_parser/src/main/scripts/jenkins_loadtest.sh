16f5f0a37a7c349b019f757cd6bc03449b00fde3
Henry Wasserman <henry.wasserman@wellogic.com>
2014-04-30 09:18:49 -0400

    Added a reports directory

--- jmeter_web_parser/src/main/scripts/jenkins_loadtest.sh
+++ jmeter_web_parser/src/main/scripts/jenkins_loadtest.sh
@@ -6,6 +6,7 @@ jmx_file=$2
 
 jmeter=/Applications/apache-jmeter-2.11/bin/jmeter.sh
 jmx_file_dir=$workspace/jmeter_web_parser/src/main/jmx_files
+report_dir=$workspace/jmeter_web_parser/src/main/reports
 
-echo $jmeter -n -t $jmx_file_dir/${jmx_file}_threaded.jmx -l $jmx_file_dir/$jmx_file.xml -Djmeter.save.saveservice.output_format=xml
-$jmeter -n -t $jmx_file_dir/${jmx_file}_threaded.jmx -l $jmx_file_dir/$jmx_file.xml -Djmeter.save.saveservice.output_format=xml
+echo $jmeter -n -t $jmx_file_dir/${jmx_file}_threaded.jmx -l $report_dir/$jmx_file.xml -Djmeter.save.saveservice.output_format=xml
+$jmeter -n -t $jmx_file_dir/${jmx_file}_threaded.jmx -l $report_dir/$jmx_file.xml -Djmeter.save.saveservice.output_format=xml
