10c054bce6900d7df320794ac066ebfd20ed621c
Henry Wasserman <henry.wasserman@wellogic.com>
2014-05-09 15:16:26 -0400

    one more time to fix piping

--- jmeter_web_parser/src/main/scripts/create_html.sh
+++ jmeter_web_parser/src/main/scripts/create_html.sh
@@ -12,8 +12,8 @@ images=$qa_tools/$workspace/jmeter_web_parser/src/main/images
 
 /bin/cp $images/*.png $report_dir
 
-echo "/usr/bin/xsltproc $xsl_dir/sort_output.xsl $report_dir/$name_of_host.xml > $report_dir/sorted_$name_of_host.xml"
-echo "/usr/bin/xsltproc $report_xsl $report_dir/sorted_$name_of_host.xml > $report_dir/$name_of_host.html"
+echo '/usr/bin/xsltproc $xsl_dir/sort_output.xsl $report_dir/$name_of_host.xml -o $report_dir/sorted_$name_of_host.xml'
+echo '/usr/bin/xsltproc $report_xsl $report_dir/sorted_$name_of_host.xml -o $report_dir/index.html'
 
 /usr/bin/xsltproc $xsl_dir/sort_output.xsl $report_dir/$name_of_host.xml -o $report_dir/sorted_$name_of_host.xml
 /usr/bin/xsltproc $report_xsl $report_dir/sorted_$name_of_host.xml -o $report_dir/index.html
