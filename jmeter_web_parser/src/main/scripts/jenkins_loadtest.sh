0a233e488cf53ac206d725953c95d1338ed84cf7
Henry Wasserman <henry.wasserman@wellogic.com>
2014-05-09 15:48:13 -0400

    also removing previous images.

--- jmeter_web_parser/src/main/scripts/jenkins_loadtest.sh
+++ jmeter_web_parser/src/main/scripts/jenkins_loadtest.sh
@@ -8,7 +8,8 @@ jmeter=/Applications/apache-jmeter-2.11/bin/jmeter.sh
 jmx_file_dir=$workspace/jmeter_web_parser/src/main/jmx_files
 report_dir=$workspace/jmeter_web_parser/src/main/reports
 
-rm report_dir/*
+rm $report_dir/*
+rm $report_dir/images/*
 
 echo $jmeter -n -t $jmx_file_dir/${jmx_file}_threaded.jmx -l $report_dir/$jmx_file.xml -Djmeter.save.saveservice.output_format=xml
 $jmeter -n -t $jmx_file_dir/${jmx_file}_threaded.jmx -l $report_dir/$jmx_file.xml -Djmeter.save.saveservice.output_format=xml
