f4a8c8a5eeb81e28b990f90506e1cf8145f9b9c0
Henry Wasserman <henry.wasserman@wellogic.com>
2014-05-09 15:45:10 -0400

    added a line to remove reports from the last run.

--- jmeter_web_parser/src/main/scripts/jenkins_loadtest.sh
+++ jmeter_web_parser/src/main/scripts/jenkins_loadtest.sh
@@ -8,5 +8,7 @@ jmeter=/Applications/apache-jmeter-2.11/bin/jmeter.sh
 jmx_file_dir=$workspace/jmeter_web_parser/src/main/jmx_files
 report_dir=$workspace/jmeter_web_parser/src/main/reports
 
+rm report_dir/*
+
 echo $jmeter -n -t $jmx_file_dir/${jmx_file}_threaded.jmx -l $report_dir/$jmx_file.xml -Djmeter.save.saveservice.output_format=xml
 $jmeter -n -t $jmx_file_dir/${jmx_file}_threaded.jmx -l $report_dir/$jmx_file.xml -Djmeter.save.saveservice.output_format=xml
